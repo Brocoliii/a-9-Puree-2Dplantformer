d70b03c86ae551ed68b9347837d7def4215cf852
Brocoliii <puree.suwa@bumail.net>
2024-09-24 11:14:20 +0700

    เพิ่ม Script
    enemy script

---
+++ Assets/ScriptLab/Ant.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Ant : Enemy
+{
+    private Vector2 velocity;
+    private Transform[] movePoint;
+    
+
+    
+    public override void Behavior()
+    {
+        
+    }
+    
+        
+    
+}
---
+++ Assets/ScriptLab/Crocodile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodite : Enemy
+{
+    private float attackRance;
+    private Player player;
+    public void Shoot()
+    {
+
+    }
+    public override void Behavior()
+    {
+        Debug.Log("Ceodile");
+    }
+}
